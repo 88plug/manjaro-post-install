fa9dd9d1cf6b707ee562235e98f9a2d3309d02e2
88plug <19512127+88plug@users.noreply.github.com>
2020-10-27 05:04:03 -0700

    Update run.sh
    add file-max

--- run.sh
+++ run.sh
@@ -75,6 +75,7 @@ sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' /home/$(cat user.log
 echo "8. Enable NTP and File Limits!"
 timedatectl set-ntp true
 echo fs.nr_open=2147483584 | tee /etc/sysctl.d/40-max-user-watches.conf
+echo fs.file-max=100000 | tee /etc/sysctl.d/40-max-user-watches.conf
 
 echo "9. Docker user setup and better options"
 groupadd docker
